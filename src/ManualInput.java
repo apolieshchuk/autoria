3b2815ccf529fc72da90355b6b95918967308ad6
apolieshchuk <anton2013ua@gmail.com>
2020-01-11 16:31:15 +0200

    Ready for manual input

--- src/ManualInput.java
+++ src/ManualInput.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class ManualInput {
 
     private static final String DB_PATH = "static/autoDb/";
     private static String auto;
